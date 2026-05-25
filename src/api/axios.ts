dc1d8eb9bd0a782f209fd1c6e7e8ecdabf9055db
10020822 <10020822@ETSPVTW-0174>
2026-05-25 13:48:21 +0530

    axios setup

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://localhost:44352/api",
   headers: {
     "Content-Type": "application/json",
   },
